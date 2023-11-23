1d06c4b204adbfebe87ea883533f4187e225db23
Nghia Lam <nghialam12795@gmail.com>
2023-11-23 23:22:24 +0700

    Combined anim setup for attacker and receiver

---
+++ Source/BatsToiSupreme_Real/Public/Animation/CombinedAnimTypes.h
@@ -0,0 +1,74 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "GameplayTagContainer.h"
+#include "CombinedAnimTypes.generated.h"
+
+UENUM(BlueprintType)
+enum class ERelativePosition : uint8
+{
+	EAny UMETA(DisplayName = "Any position"),
+	ESyncBone UMETA(DisplayName = "Get references from the sync bone"),
+};
+
+USTRUCT(BlueprintType)
+struct FCombinedAnimsAttacker : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	/* The tag of this combined anim */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Batstoi|Combined Animation")
+	FGameplayTag AnimTag;
+
+	/* The montage to be played on attacker */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Batstoi|Combined Animation")
+	class UAnimMontage* AttackerAnimMontage;
+
+	/* The receiver will be move to this position when the animation start */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Batstoi|Combined Animation")
+	ERelativePosition ReceiverForcePosition = ERelativePosition::EAny;
+};
+
+USTRUCT(BlueprintType)
+struct FCombinedAnimsReceiver : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	/* The tag of this combined anim */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Batstoi|Combined Animation")
+	FGameplayTag AnimTag;
+
+	/* The montage to be played on receiver */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Batstoi|Combined Animation")
+	class UAnimMontage* ReceiverAnimMontage;
+};
+
+USTRUCT(BlueprintType)
+struct FCombinedAnim
+{
+	GENERATED_BODY()
+
+public:
+	FCombinedAnim() {};
+
+	FCombinedAnim(const FCombinedAnimsAttacker& InAttackerConfig, const FGameplayTag& InTag, ACharacter* InCharacterRef)
+	{
+		AttackerAnimConfig = InAttackerConfig;
+		AnimTag = InTag;
+		ReceiverCharacterRef = InCharacterRef;
+	}
+
+	UPROPERTY(BlueprintReadOnly, Category = "Batstoi|Combined Animation")
+	FCombinedAnimsAttacker AttackerAnimConfig;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Batstoi|Combined Animation")
+	FGameplayTag AnimTag;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Batstoi|Combined Animation")
+	TObjectPtr<ACharacter> ReceiverCharacterRef;
+};
