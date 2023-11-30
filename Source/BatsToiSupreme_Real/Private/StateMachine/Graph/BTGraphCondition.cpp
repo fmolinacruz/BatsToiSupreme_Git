7a73dc211734ce6f24394078070109f6b4d29fc5
Nghia Lam <nghia.lt@gianty.com.vn>
2023-11-30 15:56:53 +0700

    StateMachine: Condition implementation

--- Source/BatsToiSupreme_Real/Private/StateMachine/Graph/BTGraphCondition.cpp
+++ Source/BatsToiSupreme_Real/Private/StateMachine/Graph/BTGraphCondition.cpp
@@ -1,3 +1,46 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "StateMachine/Graph/BTGraphCondition.h"
+
+bool UBTGraphCondition::Verify(APlayerController* PlayerController)
+{
+	Controller = PlayerController;
+	VerifyCondition(PlayerController);
+}
+
+bool UBTGraphCondition::VerifyCondition_Implementation(APlayerController* PlayerController) const
+{
+	return true;
+}
+
+/* Condition OR *****************************************************/
+
+UBTGraphOrCondition::UBTGraphOrCondition() {}
+
+bool UBTGraphOrCondition::VerifyCondition_Implementation(APlayerController* PlayerController) const
+{
+	for(UBTGraphCondition* Condition : OrConditions)
+	{
+		if (Condition->Verify(Controller))
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+/* Condition AND *****************************************************/
+
+UBTGraphAndCondition::UBTGraphAndCondition() {}
+
+bool UBTGraphAndCondition::VerifyCondition_Implementation(APlayerController* PlayerController) const
+{
+	for(UBTGraphCondition* Condition : AndConditions)
+	{
+		if (!Condition->Verify(Controller))
+		{
+			return false;
+		}
+	}
+	return true;
+}
--- Source/BatsToiSupreme_Real/Public/StateMachine/Graph/BTGraphCondition.h
+++ Source/BatsToiSupreme_Real/Public/StateMachine/Graph/BTGraphCondition.h
@@ -6,8 +6,56 @@
 #include "UObject/Object.h"
 #include "BTGraphCondition.generated.h"
 
-UCLASS()
+UCLASS(Blueprintable, BlueprintType, abstract, EditInlineNew, HideCategories = ("DoNotShow"), CollapseCategories, AutoExpandCategories = ("Default"))
 class BATSTOISUPREME_REAL_API UBTGraphCondition : public UObject
 {
 	GENERATED_BODY()
+
+public:
+	bool Verify(APlayerController* PlayerController);
+
+protected:
+	UFUNCTION(BlueprintNativeEvent, Category = "BTGraph|Condition")
+	bool VerifyCondition(APlayerController* PlayerController) const;
+
+	virtual bool VerifyCondition_Implementation(APlayerController* PlayerController) const;
+
+	UPROPERTY(BlueprintReadOnly, Category = "BTGraph|Condition")
+	TObjectPtr<APlayerController> Controller;
+};
+
+/**
+ * Condition OR
+ */
+UCLASS(NotBlueprintable, BlueprintType, EditInlineNew, HideCategories = ("DoNotShow"), CollapseCategories, AutoExpandCategories = ("Default"))
+class BATSTOISUPREME_REAL_API UBTGraphOrCondition : public UBTGraphCondition
+{
+	GENERATED_BODY()
+
+public:
+	UBTGraphOrCondition();
+
+protected:
+	virtual bool VerifyCondition_Implementation(APlayerController* PlayerController) const override;
+
+	UPROPERTY(Instanced, EditDefaultsOnly, Category = "BTGraph|Condition")
+	TArray<TObjectPtr<UBTGraphCondition>> OrConditions;
+};
+
+/**
+ * Condition AND
+ */
+UCLASS(NotBlueprintable, BlueprintType, EditInlineNew, HideCategories = ("DoNotShow"), CollapseCategories, AutoExpandCategories = ("Default"))
+class BATSTOISUPREME_REAL_API UBTGraphAndCondition : public UBTGraphCondition
+{
+	GENERATED_BODY()
+	
+public:
+	UBTGraphAndCondition();
+
+protected:
+	virtual bool VerifyCondition_Implementation(APlayerController* PlayerController) const override;
+
+	UPROPERTY(Instanced, EditDefaultsOnly, Category = "BTGraph|Condition")
+	TArray<TObjectPtr<UBTGraphCondition>> AndConditions;
 };
