50844feaf0c55ffc007880d6f4f059e48ac98dc7
DuyNguyen <100117127+DuyNguyenMangoX@users.noreply.github.com>
2023-12-22 17:35:40 +0700

    Select Player by keyboard

--- Source/BatsToiSupreme_Real/Private/Input/InputReceive.cpp
+++ Source/BatsToiSupreme_Real/Private/Input/InputReceive.cpp
@@ -7,28 +7,8 @@
 AInputReceive::AInputReceive()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 }
 
-// Called when the game starts or when spawned
-void AInputReceive::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void AInputReceive::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-// Called to bind functionality to input
-void AInputReceive::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
 
