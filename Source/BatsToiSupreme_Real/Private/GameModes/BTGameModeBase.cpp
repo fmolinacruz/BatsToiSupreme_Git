0cafa0c52fa4fda08c517b7bc98da8f55aca8c85
Nghia Lam <nghia.lt@gianty.com.vn>
2024-01-30 16:43:11 +0700

    New server implementation

--- Source/BatsToiSupreme_Real/Private/GameModes/BTGameModeBase.cpp
+++ Source/BatsToiSupreme_Real/Private/GameModes/BTGameModeBase.cpp
@@ -1,7 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "GameModes/BTGameModeBase.h"
-// #include "GameMapsSetting.h"
 
 #include "Kismet/GameplayStatics.h"
 #include "Characters/BTBaseCharacter.h"
@@ -9,11 +8,14 @@
 #include "PlayerCommon/BTPlayerController.h"
 #include "Utilities/BTLogging.h"
 
-#include "Menu/WBTMenu.h"
-
 ABTGameModeBase::ABTGameModeBase(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer), MainCameraRef(nullptr)
 {
+	GameLiftSDKModule = nullptr;
+	GameLiftProcessParams.OnStartGameSession.BindUObject(this, &ABTGameModeBase::OnGameLiftSessionStart);
+	GameLiftProcessParams.OnUpdateGameSession.BindUObject(this, &ABTGameModeBase::OnGameLiftSessionUpdate);
+	GameLiftProcessParams.OnTerminate.BindUObject(this, &ABTGameModeBase::OnGameLiftProcessTerminate);
+	GameLiftProcessParams.OnHealthCheck.BindUObject(this, &ABTGameModeBase::OnGameLiftServerHealthCheck);
 }
 
 void ABTGameModeBase::BeginPlay()
@@ -30,89 +32,66 @@ void ABTGameModeBase::InitGameLift()
 	BTLOG_DISPLAY("Initialize GameLift Server !");
 
 	// Getting the module first.
-	FGameLiftServerSDKModule* GameLiftSdkModule = &FModuleManager::LoadModuleChecked<FGameLiftServerSDKModule>(FName("GameLiftServerSDK"));
+	GameLiftSDKModule = &FModuleManager::LoadModuleChecked<FGameLiftServerSDKModule>(FName("GameLiftServerSDK"));
 
-	// Define the server parameters for a GameLift Anywhere fleet. These are not needed for a GameLift managed EC2 fleet.
-	FServerParameters ServerParameters;
-
-	// AuthToken returned from the "aws gamelift get-compute-auth-token" API. Note this will expire and require a new call to the API after 15 minutes.
-	if (FParse::Value(FCommandLine::Get(), TEXT("-authtoken="), ServerParameters.m_authToken))
+	/*
+	if (FParse::Value(FCommandLine::Get(), TEXT("-authtoken="), GameLiftServerParams.m_authToken))
 	{
-		BTLOG_DISPLAY("AUTH_TOKEN: %s", *ServerParameters.m_authToken);
+		BTLOG_DISPLAY("AUTH_TOKEN: %s", *GameLiftServerParams.m_authToken);
+	}
+	if (FParse::Value(FCommandLine::Get(), TEXT("-hostid="), GameLiftServerParams.m_hostId))
+	{
+		BTLOG_DISPLAY("HOST_ID: %s", GameLiftServerParams.m_hostId);
+	}
+	if (FParse::Value(FCommandLine::Get(), TEXT("-fleetid="), GameLiftServerParams.m_fleetId))
+	{
+		BTLOG_DISPLAY("FLEET_ID: %s", *GameLiftServerParams.m_fleetId);
+	}
+	if (FParse::Value(FCommandLine::Get(), TEXT("-websocketurl="), GameLiftServerParams.m_webSocketUrl))
+	{
+		BTLOG_DISPLAY("WEBSOCKET_URL: %s", *GameLiftServerParams.m_webSocketUrl);
 	}
 
-	// The Host/compute-name of the GameLift Anywhere instance.
-	if (FParse::Value(FCommandLine::Get(), TEXT("-hostid="), ServerParameters.m_hostId))
-	{
-		BTLOG_DISPLAY("HOST_ID: %s", *ServerParameters.m_hostId);
-	}
+	GameLiftServerParams.m_processId = FString::Printf(TEXT("%d"), GetCurrentProcessId());
+	BTLOG_DISPLAY("PID: %s", *GameLiftServerParams.m_processId);
+	*/
 
-	// The Anywhere Fleet ID.
-	if (FParse::Value(FCommandLine::Get(), TEXT("-fleetid="), ServerParameters.m_fleetId))
-	{
-		BTLOG_DISPLAY("FLEET_ID: %s", *ServerParameters.m_fleetId);
-	}
+	// GameLiftSDKModule->InitSDK(GameLiftServerParams);
+	GameLiftSDKModule->InitSDK();
 
-	// The WebSocket URL (GameLiftServiceSdkEndpoint).
-	if (FParse::Value(FCommandLine::Get(), TEXT("-websocketurl="), ServerParameters.m_webSocketUrl))
-	{
-		BTLOG_DISPLAY("WEBSOCKET_URL: %s", *ServerParameters.m_webSocketUrl);
-	}
-
-	// The PID of the running process
-	ServerParameters.m_processId = FString::Printf(TEXT("%d"), GetCurrentProcessId());
-	BTLOG_DISPLAY("PID: %s", *ServerParameters.m_processId);
-
-	// InitSDK establishes a local connection with GameLift's agent to enable further communication.
-	// Use InitSDK(serverParameters) for a GameLift Anywhere fleet.
-	// Use InitSDK() for a GameLift managed EC2 fleet.
-	GameLiftSdkModule->InitSDK(ServerParameters);
-
-	// Implement callback function onStartGameSession
-	// GameLift sends a game session activation request to the game server and passes a game session object with game properties and other settings.
-	// Here is where a game server takes action based on the game session object. When the game server is ready to receive incoming player connections,
-	// it invokes the server SDK call ActivateGameSession().
-	auto OnGameSession = [=](const Aws::GameLift::Server::Model::GameSession& GameLiftSession) {
-		const FString GameSessionId = FString(GameLiftSession.GetGameSessionId());
-		BTLOG_DISPLAY("GameSession Initializing: %s", *GameSessionId);
-		GameLiftSdkModule->ActivateGameSession();
-	};
-
-	GameLiftParams.OnStartGameSession.BindLambda(OnGameSession);
-
-	// Implement callback function OnProcessTerminate
-	// GameLift invokes this callback before shutting down the instance hosting this game server. It gives the game server a chance to save its state, communicate with services, etc.,
-	// and initiate shut down. When the game server is ready to shut down, it invokes the server SDK call ProcessEnding() to tell GameLift it is shutting down.
-	auto onProcessTerminate = [=]() {
-		BTLOG_DISPLAY("Game Server Process is terminating!");
-		GameLiftSdkModule->ProcessEnding();
-	};
-
-	GameLiftParams.OnTerminate.BindLambda(onProcessTerminate);
-
-	// Implement callback function OnHealthCheck
-	// GameLift invokes this callback approximately every 60 seconds. A game server might want to check the health of dependencies, etc.
-	// Then it returns health status true if healthy, false otherwise. The game server must respond within 60 seconds, or GameLift records 'false'.
-	// In this example, the game server always reports healthy.
-	auto onHealthCheck = []() {
-		BTLOG_DISPLAY("Performing Health Check");
-		return true;
-	};
-
-	GameLiftParams.OnHealthCheck.BindLambda(onHealthCheck);
-
-	// The game server gets ready to report that it is ready to host game sessions and that it will listen on port 7777 for incoming player connections.
-	GameLiftParams.port = 7777;
-
-	// Here, the game server tells GameLift where to find game session log files. At the end of a game session, GameLift uploads everything in the specified
-	// location and stores it in the cloud for access later.
-	TArray<FString> logfiles;
-	logfiles.Add(TEXT("GameLift426Test/Saved/Logs/GameLift426Test.log"));
-	GameLiftParams.logParameters = logfiles;
-
-	// The game server calls ProcessReady() to tell GameLift it's ready to host game sessions.
+	GameLiftProcessParams.port = 7777;
+	
+	TArray<FString> Logfiles;
+	Logfiles.Add(TEXT("GameLiftServer/Saved/Logs/GameLiftTest.log"));
+	GameLiftProcessParams.logParameters = Logfiles;
+	
 	BTLOG_DISPLAY("Calling Process Ready");
-	GameLiftSdkModule->ProcessReady(GameLiftParams);
+	GameLiftSDKModule->ProcessReady(GameLiftProcessParams);
+}
+
+void ABTGameModeBase::OnGameLiftSessionStart(Aws::GameLift::Server::Model::GameSession ActivatedSession)
+{
+	const FString GameSessionId = FString(ActivatedSession.GetGameSessionId());
+	BTLOG_DISPLAY("GameSession Initializing: %s", *GameSessionId);
+
+	GameLiftSDKModule->ActivateGameSession();
+}
+
+void ABTGameModeBase::OnGameLiftSessionUpdate(Aws::GameLift::Server::Model::UpdateGameSession UpdatedSession)
+{
+	BTLOG_DISPLAY("GameSession Updating!");
+}
+
+void ABTGameModeBase::OnGameLiftProcessTerminate()
+{
+	BTLOG_DISPLAY("Game Server Process is terminating!");
+	GameLiftSDKModule->ProcessEnding();
+}
+
+bool ABTGameModeBase::OnGameLiftServerHealthCheck()
+{
+	BTLOG_DISPLAY("Performing Health Check");
+	return true;
 }
 
 void ABTGameModeBase::OnPostLogin(AController* NewPlayer)
@@ -153,7 +132,7 @@ void ABTGameModeBase::OnPostLogin(AController* NewPlayer)
 		// Initialize the InputReceiver with the player controller and current player index
 		InputReceivers.Add(InputReceiver);
 		InputReceiver->InitializeWithPlayerController(PC, CurrentPlayerIndex);
-		
+
 		if (InputReceivers.Num() >= 2)
 		{
 			InputReceivers[0]->OtherPlayerController = InputReceivers[1]->CurrentPlayerController;
@@ -256,4 +235,3 @@ void ABTGameModeBase::GetStartSpots()
 {
 	UGameplayStatics::GetAllActorsOfClass(GetWorld(), APlayerStart::StaticClass(), StartSpots);
 }
-
