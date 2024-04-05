217ad72261129e32d03789d3314b63afa115584c
DuyNguyen <100117127+DuyNguyenMangoX@users.noreply.github.com>
2024-04-05 17:21:15 +0700

    Varest http Request: Create, get status

---
+++ Source/BatsToiSupreme_Real/Public/Utilities/VaRestHttpRequest.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class BATSTOISUPREME_REAL_API VaRestHttpRequest
+{
+public:
+	VaRestHttpRequest();
+	~VaRestHttpRequest();
+
+	void MatchMakingCreate();
+	void MatchMakingGetStatus();
+
+	UVaRestSubsystem* GetVARestSub();
+
+private:
+	FString HostURL;
+};
