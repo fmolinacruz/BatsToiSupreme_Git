c429a3df565de3da911ee157045c53cc95ea84e2
Duy Nguyen <duy.np@gianty.com.vn>
2024-04-08 20:53:55 +0700

    Remove GASCompanionTest

---
+++ Command/clean-pdb.bat
@@ -0,0 +1,6 @@
+
+REM Clean pdb
+set "src=.\..\WindowsServer\BatsToiSupreme_Real\Binaries\Win64\BatsToiSupreme_RealServer.pdb"
+echo start Delete file
+
+del /q %src%
--- Plugins/GASCompanionTests/Source/GASCompanionTests/Private/GASCompanionTestsNativeTags.cpp
+++
@@ -1,6 +0,0 @@
-﻿// Copyright 2020 Mickael Daniel. All Rights Reserved.
-
-
-#include "GASCompanionTestsNativeTags.h"
-
-FGASCompanionTestsNativeTags FGASCompanionTestsNativeTags::NativeTags;
