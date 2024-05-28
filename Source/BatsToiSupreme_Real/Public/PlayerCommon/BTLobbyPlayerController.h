86fa8ff1e668067a5acc37e65f8fa0ea6272316e
Duy Nguyen <duy.np@gianty.com.vn>
2024-05-28 16:32:17 +0700

    Login Code

--- Source/BatsToiSupreme_Real/Public/PlayerCommon/BTLobbyPlayerController.h
+++ Source/BatsToiSupreme_Real/Public/PlayerCommon/BTLobbyPlayerController.h
@@ -17,4 +17,14 @@ class BATSTOISUPREME_REAL_API ABTLobbyPlayerController : public APlayerControlle
 public:
 	UFUNCTION(BlueprintCallable, Category = "Batstoi|Utils")
 	FString GetDeviceId();
+
+	// Function to sign into EOS Game Services
+	UFUNCTION(BlueprintCallable, Category = "Batstoi|EOS")
+	void EOSLogin();
+
+	// Callback function. This function is ran when signing into EOS Game Services completes. 
+	void OnEOSLoginCompleted(int32 LocalUserNum, bool bWasSuccessful, const FUniqueNetId& UserId, const FString& Error);
+
+	// Delegate to bind callback event for login.
+	FDelegateHandle LoginDelegateHandle;
 };
